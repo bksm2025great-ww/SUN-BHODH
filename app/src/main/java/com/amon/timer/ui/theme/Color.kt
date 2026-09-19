88b664526c004f95105029fc9e3b809c7f833ecb
bhanu sharma <bksm2025great@gmail.com>
2026-09-19 06:52:55 +0530

    Create Color.kt

---
+++ app/src/main/java/com/amon/timer/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.amon.timer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 🟢 START: [AMON_COLOR_PALETTE]
+val DarkBackground = Color(0xFF0F0F12)
+val DarkSurface = Color(0xFF1B1B1F)
+val DarkSurfaceVariant = Color(0xFF2B2B30)
+
+val AccentYellow = Color(0xFFFBBF24)
+val AccentYellowGlow = Color(0x33FBBF24)
+
+val TextWhite = Color(0xFFEEEEEE)
+val TextMuted = Color(0xFF888890)
+// 🔴 END: [AMON_COLOR_PALETTE]
