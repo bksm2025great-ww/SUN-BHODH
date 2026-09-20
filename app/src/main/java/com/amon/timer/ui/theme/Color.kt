fb0e668aa664ae4fdcf6eb67a4fd5f0cefaddddd
bhanu sharma <bksm2025great@gmail.com>
2026-09-20 19:21:09 +0530

    Update Color.kt

--- app/src/main/java/com/amon/timer/ui/theme/Color.kt
+++ app/src/main/java/com/amon/timer/ui/theme/Color.kt
@@ -7,8 +7,15 @@ val DarkBackground = Color(0xFF0F0F12)
 val DarkSurface = Color(0xFF1B1B1F)
 val DarkSurfaceVariant = Color(0xFF2B2B30)
 
+// 🟡 Classic Yellow (आपका पुराना रंग)
 val AccentYellow = Color(0xFFFBBF24)
 val AccentYellowGlow = Color(0x33FBBF24)
+
+// 👑 Luxe Gold (नया प्रीमियम रंग)
+val LuxeGold = Color(0xFFF3C669)
+val LuxeGoldGlow = Color(0x33F3C669)
+val LuxeGoldDark = Color(0xFFE5B869)
+val DeepGold = Color(0xFF8C6E33)
 
 val TextWhite = Color(0xFFEEEEEE)
 val TextMuted = Color(0xFF888890)
