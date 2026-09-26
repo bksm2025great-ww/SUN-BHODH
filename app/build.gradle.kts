830e33c8768e8fccdabed89c6497030be4c68c39
bhanu sharma <bksm2025great@gmail.com>
2026-09-26 10:44:29 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vision.amon"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         vectorDrawables {
             useSupportLibrary = true
